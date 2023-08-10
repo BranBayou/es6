dbca15b54900557db0c8b7e460635ecd717c63c4
BranBayou <berhan.baye@gmail.com>
2023-08-10 21:47:16 +0300

    Array with for loop

--- script.js
+++ script.js
@@ -52,3 +52,18 @@ function checkWinner(AvgScoreTeam1, avgScoreTeam2) {
 }
 
 checkWinner(scoreTeam1, scoreTeam2);
+
+function calcTip(bills) {
+    return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
+}  
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i])
+}
+console.log(bills, tips, totals);
