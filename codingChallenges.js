b770bb466c3c01c83e70ccc16055e76f17176757
BranBayou <berhan.baye@gmail.com>
2023-08-16 20:48:32 +0300

    Add challenge solution

---
+++ codingChallenges.js
@@ -0,0 +1,63 @@
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussis Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ]
+  ],
+  score:'4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5
+  },
+};
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'Tiango', 'Coutinho', 'Periscic'];
+console.log(players1Final);
+
+const {odds: {team1, x:draw, team2}} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function(...players) {
+  console.log(`${players.length} goals were scored`);
+}
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+team1 < team2 && console.log('Team 1 is more likely to win!');
