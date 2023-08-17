8776ef7cb13e83e6bcc20099259cf01a872c0081
BranBayou <berhan.baye@gmail.com>
2023-08-17 11:14:30 +0300

    Add solution

--- codingChallenges.js
+++ codingChallenges.js
@@ -77,3 +77,33 @@ for(const [team, odd] of Object.entries(game.odds)){
   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
   console.log(`Odd of ${teamStr} ${odd}`);
 }
+
+//Coding Challenge Three
+
+const gameEvents = new Map([
+  [17, 'Goal'],
+  [36, 'Substitution'],
+  [47, 'Goal'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'Goal'],
+  [80, 'Goal'],
+  [92, 'Yellow card']
+]);
+console.log(gameEvents.values());
+const events = new Set(gameEvents.values());
+console.log(events);
+const eventsArray = [...new Set(gameEvents.values())];
+console.log(eventsArray);
+
+gameEvents.delete(64);
+
+console.log(`An event happened, on average, dvery ${90/gameEvents.size} minutes`);
+
+for(const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND'; 
+  console.log(`[${half}HALF] ${min} ${event}`);
+}
