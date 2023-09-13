7bd4ab46c25ba5e17b5a9c5d9d748054ab167e1f
BranBayou <berhan.baye@gmail.com>
2023-09-13 20:33:33 +0300

    Add undefined and not defined error

--- error.js
+++ error.js
@@ -1,3 +1,5 @@
+// 'use strict';
+
 // Syntax Errors from the Start
 
 // var greeting = "Hello";
@@ -28,18 +30,46 @@
 //   saySomething();
 //   // ReferenceErro
 
-var students = [
-  { id: 14, name: "Kyle" },
-  { id: 73, name: "Suzy" },
-  { id: 112, name: "Frank" },
-  { id: 6, name: "Sarah" },
-];
-function getStudentName(studentID) {
-  for (let student of students) {
-    if (student.id == studentID) {
-      return student.name;
-    }
-  }
-}
-var nextStudent = getStudentName(112);
-console.log(nextStudent);
+
+// Outer/ Global scope: RED
+// var students = [
+//   { id: 14, name: "Kyle" },
+//   { id: 73, name: "Suzy" },
+//   { id: 112, name: "Frank" },
+//   { id: 6, name: "Sarah" },
+// ];
+// function getStudentName(studentID) {
+//   //function scope: BLUE
+//   for (let student of students) {
+//     // Loop scope: GREEN
+//     if (student.id == studentID) {
+//       return student.name;
+//     }
+//   }
+// }
+// var nextStudent = getStudentName(112);
+// console.log(nextStudent);
+
+// var badIdea = {oops: 'Ugh'};
+// with(badIdea) {
+//   console.log(oops)
+// }
+
+//Undefined error vs Not defined error
+
+// var studentName;
+// console.log(typeof(studentName)); // undefined error
+// console.log(typeof(doesntExist)); // undefined error
+
+// console.log(stuName); // Not defined error
+
+// Accidental Global Variable
+
+// function getStudentName() {
+//   // assignment to an undeclared variable :(
+//   nextStudent = "Suzy";
+//   }
+//   getStudentName();
+//   console.log(nextStudent);
+  // "Suzy" -- oops, an accidental-global variable!
+  // This sort of accident almost certain to lead to bugs eventually
