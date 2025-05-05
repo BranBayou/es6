e78e6c99e36803490247f101ffb86d42688f17b5
Berhanu Baye <berhan.baye@gmail.com>
2025-05-05 14:21:47 +0300

    Nullish Coalescing Operato

--- es2020.js
+++ es2020.js
@@ -6,9 +6,23 @@ const iterator = text.matchAll(/string/g);
 const pTag = document.querySelector('.matchAll');
 const button = document.querySelector('.show-match');
 
+const pTag2 = document.querySelector('.nullish');
+const checkNulishBtn = document.querySelector('.show-nullish');
+
 button.addEventListener('click', matchAll);
+checkNulishBtn.addEventListener('click', showNullish);
 
 function matchAll () {
     console.log('Triggered matchAll()');
   pTag.innerHTML = Array.from(iterator);
+}
+
+//JavaScript Nullish Coalescing Operator (??)
+let name = null;
+let text1 = "missing";
+let result = name ?? text1;
+
+function showNullish() {
+    console.log('Triggered Nullish Coalescing Operator (??)');
+    pTag2.innerHTML = result;
 }
