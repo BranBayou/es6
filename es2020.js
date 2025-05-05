668a2b519de773bd6784a5a1363aad447f72813a
Berhanu Baye <berhan.baye@gmail.com>
2025-05-05 14:29:10 +0300

    Optional Chaining Operator

--- es2020.js
+++ es2020.js
@@ -1,5 +1,4 @@
 //JavaScript String matchAll()
-
 const text = 'this is a test string and this is another test string';
 const iterator = text.matchAll(/string/g);
 
@@ -9,8 +8,12 @@ const button = document.querySelector('.show-match');
 const pTag2 = document.querySelector('.nullish');
 const checkNulishBtn = document.querySelector('.show-nullish');
 
+const pTag3 = document.querySelector('.optional-chaining');
+const checkOptionalChainingBtn = document.querySelector('.show-optional-chaining');
+
 button.addEventListener('click', matchAll);
 checkNulishBtn.addEventListener('click', showNullish);
+checkOptionalChainingBtn.addEventListener('click', checkOptionalChaining);
 
 function matchAll () {
     console.log('Triggered matchAll()');
@@ -25,4 +28,19 @@ let result = name ?? text1;
 function showNullish() {
     console.log('Triggered Nullish Coalescing Operator (??)');
     pTag2.innerHTML = result;
+}
+
+// JavaScript Optional Chaining Operator (?.)
+const user = {
+    name: 'John Doe',
+    address: {
+        street: '123 Main St',
+        city: 'New York',
+        country: 'USA'
+    }
+};
+
+function checkOptionalChaining() {
+    console.log('Triggered Optional Chaining Operator (?.)');
+    pTag3.innerHTML = user?.address?.postal ?? 'Street not found';
 }
