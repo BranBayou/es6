e55f09b3685729695cb5b99f8e43831ff2bb5ecb
Berhanu Baye <berhan.baye@gmail.com>
2025-07-03 22:50:52 +0300

    Promise more example

--- es2020.js
+++ es2020.js
@@ -136,3 +136,30 @@ async function mani() {
     console.log('Third');
 }
 mani();
+
+// Promises example 3
+async function showAvatar() {
+
+  // read our JSON
+  let response = await fetch('/article/promise-chaining/user.json');
+  let user = await response.json();
+
+  // read github user
+  let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+  let githubUser = await githubResponse.json();
+
+  // show the avatar
+  let img = document.createElement('img');
+  img.src = githubUser.avatar_url;
+  img.className = "promise-avatar-example";
+  document.body.append(img);
+
+  // wait 3 seconds
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  img.remove();
+
+  return githubUser;
+}
+
+showAvatar();
