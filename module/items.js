2acd6c584b0fa364995e4d4c25e47c4bab3131aa
Berhanu Baye <berhan.baye@gmail.com>
2025-12-07 18:34:28 +0300

    show items availability

---
+++ module/items.js
@@ -0,0 +1,202 @@
+export const item = {
+    "Options": [
+        {
+            "Alias": "actualcolour",
+            "Name": "Colour",
+            "TemplateName": "sb-productselector-color",
+            "ListingTemplateName": "",
+            "IsSize": false,
+            "IsColour": true,
+            "HasMultipleValues": true,
+            "Values": [
+                {
+                    "Alias": "no-photo-available.jpg",
+                    "Value": "White",
+                    "ValueAlias": "White"
+                },
+                {
+                    "Alias": "no-photo-available.jpg",
+                    "Value": "Blue",
+                    "ValueAlias": "Blue"
+                },
+                {
+                    "Alias": "no-photo-available.jpg",
+                    "Value": "Black",
+                    "ValueAlias": "Black"
+                }
+            ]
+        },
+        {
+            "Alias": "sizepantswaist",
+            "Name": "Size - Pants - Waist",
+            "TemplateName": "sb-productselector-size",
+            "ListingTemplateName": "",
+            "IsSize": false,
+            "IsColour": false,
+            "HasMultipleValues": true,
+            "Values": [
+                {
+                    "Alias": "no-photo-available.jpg",
+                    "Value": "Waist 32",
+                    "ValueAlias": "Waist32"
+                },
+                {
+                    "Alias": "no-photo-available.jpg",
+                    "Value": "Waist 30",
+                    "ValueAlias": "Waist30"
+                },
+                {
+                    "Alias": "no-photo-available.jpg",
+                    "Value": "Waist 31",
+                    "ValueAlias": "Waist31"
+                }
+            ]
+        },
+        {
+            "Alias": "sizepantslength",
+            "Name": "Size - Pants - Length",
+            "TemplateName": "sb-productselector-size",
+            "ListingTemplateName": "",
+            "IsSize": false,
+            "IsColour": false,
+            "HasMultipleValues": true,
+            "Values": [
+                {
+                    "Alias": "no-photo-available.jpg",
+                    "Value": "30",
+                    "ValueAlias": "30"
+                },
+                {
+                    "Alias": "no-photo-available.jpg",
+                    "Value": "32",
+                    "ValueAlias": "32"
+                }
+            ]
+        }
+    ],
+    "Items": [
+        {
+            "Description": "<p>Introducing the Rothco Frontline Series Edge Pants, meticulously designed for the modern adventurer and tactical enthusiast. These functional pants combine comfort with effectiveness, providing durability in all terrains. Crafted from high-quality materials, they are perfect for outdoor excursions, casual wear, or tactical scenarios. Features include multiple pockets for secure storage of essentials, adjustable waist for a perfect fit, and a stylish, yet practical design. Whether you are hiking through the wilderness or tackling everyday challenges, the Edge Pants keep you equipped and ready for anything. Your gear should work as hard as you do!</p>",
+            "fullRetailSku": "UF77730-01-berhanu-Wht-32-30",
+            "sizeChart": "",
+            "actualcolour": "White",
+            "sizepantswaist": "Waist 32",
+            "sizepantslength": "30",
+            "images": [],
+            "Id": "4f1698e1-2113-42b1-b7a2-b3a901300672",
+            "ProductId": "9e5b2673-ef19-4263-8c8a-b3a901300657",
+            "FullSku": "UF77730-01-berhanu-Wht-32-30",
+            "Name": "Ber test Rothco Frontline Series Edge Pants-Wht-32-30",
+            "DisplayOrder": 0,
+            "AvailableForPurchase": false,
+            "HideFromListingPage": false,
+            "HideFromPurchaseForms": false,
+            "AvailabilityText": "Out of Stock",
+            "BuyButtonText": "Out of Stock",
+            "Price": 99.00,
+            "PriceAmount": "$99.00",
+            "OriginalPrice": 99.00,
+            "OriginalPriceAmount": "$99.00",
+            "InventoryAvailable": 0
+        },
+        {
+            "Description": "<p>Introducing the Rothco Frontline Series Edge Pants, meticulously designed for the modern adventurer and tactical enthusiast. These functional pants combine comfort with effectiveness, providing durability in all terrains. Crafted from high-quality materials, they are perfect for outdoor excursions, casual wear, or tactical scenarios. Features include multiple pockets for secure storage of essentials, adjustable waist for a perfect fit, and a stylish, yet practical design. Whether you are hiking through the wilderness or tackling everyday challenges, the Edge Pants keep you equipped and ready for anything. Your gear should work as hard as you do!</p>",
+            "fullRetailSku": "ber-retailSku-1",
+            "sizeChart": "",
+            "actualcolour": "White",
+            "sizepantswaist": "Waist 30",
+            "sizepantslength": "30",
+            "images": [],
+            "Id": "c47706f6-ca57-4b10-a493-b3a9013357a2",
+            "ProductId": "9e5b2673-ef19-4263-8c8a-b3a901300657",
+            "FullSku": "ber-retailSku-1",
+            "Name": "Ber test Rothco Frontline Series Edge Pants-Wht-30-30",
+            "DisplayOrder": 0,
+            "AvailableForPurchase": false,
+            "HideFromListingPage": false,
+            "HideFromPurchaseForms": false,
+            "AvailabilityText": "Out of Stock",
+            "BuyButtonText": "Out of Stock",
+            "Price": 150.00,
+            "PriceAmount": "$150.00",
+            "OriginalPrice": 150.00,
+            "OriginalPriceAmount": "$150.00",
+            "InventoryAvailable": 0
+        },
+        {
+            "Description": "<p>Introducing the Rothco Frontline Series Edge Pants, meticulously designed for the modern adventurer and tactical enthusiast. These functional pants combine comfort with effectiveness, providing durability in all terrains. Crafted from high-quality materials, they are perfect for outdoor excursions, casual wear, or tactical scenarios. Features include multiple pockets for secure storage of essentials, adjustable waist for a perfect fit, and a stylish, yet practical design. Whether you are hiking through the wilderness or tackling everyday challenges, the Edge Pants keep you equipped and ready for anything. Your gear should work as hard as you do!</p>",
+            "fullRetailSku": "UF77730-01-berhanu-blu-31-32",
+            "sizeChart": "",
+            "actualcolour": "Blue",
+            "sizepantswaist": "Waist 31",
+            "sizepantslength": "32",
+            "images": [],
+            "Id": "b32885c6-79a4-4179-ad1d-b3a90133ed52",
+            "ProductId": "9e5b2673-ef19-4263-8c8a-b3a901300657",
+            "FullSku": "UF77730-01-berhanu-blu-31-32",
+            "Name": "Ber test Rothco Frontline Series Edge Pants-blu-31-32",
+            "DisplayOrder": 3,
+            "AvailableForPurchase": false,
+            "HideFromListingPage": false,
+            "HideFromPurchaseForms": false,
+            "AvailabilityText": "Out of Stock",
+            "BuyButtonText": "Out of Stock",
+            "Price": 230.00,
+            "PriceAmount": "$230.00",
+            "OriginalPrice": 250.00,
+            "OriginalPriceAmount": "$250.00",
+            "SalePrice": 230.00,
+            "SalePriceAmount": "$230.00",
+            "SalePercentage": "8% OFF",
+            "InventoryAvailable": 0
+        },
+        {
+            "Description": "<p>Introducing the Rothco Frontline Series Edge Pants, meticulously designed for the modern adventurer and tactical enthusiast. These functional pants combine comfort with effectiveness, providing durability in all terrains. Crafted from high-quality materials, they are perfect for outdoor excursions, casual wear, or tactical scenarios. Features include multiple pockets for secure storage of essentials, adjustable waist for a perfect fit, and a stylish, yet practical design. Whether you are hiking through the wilderness or tackling everyday challenges, the Edge Pants keep you equipped and ready for anything. Your gear should work as hard as you do!</p>",
+            "fullRetailSku": "ber-retailSku-3",
+            "sizeChart": "",
+            "actualcolour": "Black",
+            "sizepantswaist": "Waist 30",
+            "sizepantslength": "30",
+            "images": [],
+            "Id": "3fa10458-ae86-4943-b91d-b3a9013355a3",
+            "ProductId": "9e5b2673-ef19-4263-8c8a-b3a901300657",
+            "FullSku": "ber-retailSku-3",
+            "Name": "Ber test Rothco Frontline Series Edge Pants-Blk-30-30",
+            "DisplayOrder": 4,
+            "AvailableForPurchase": false,
+            "HideFromListingPage": false,
+            "HideFromPurchaseForms": false,
+            "AvailabilityText": "Out of Stock",
+            "BuyButtonText": "Out of Stock",
+            "Price": 100.00,
+            "PriceAmount": "$100.00",
+            "OriginalPrice": 100.00,
+            "OriginalPriceAmount": "$100.00",
+            "InventoryAvailable": 0
+        },
+        {
+            "Description": "<p>Introducing the Rothco Frontline Series Edge Pants, meticulously designed for the modern adventurer and tactical enthusiast. These functional pants combine comfort with effectiveness, providing durability in all terrains. Crafted from high-quality materials, they are perfect for outdoor excursions, casual wear, or tactical scenarios. Features include multiple pockets for secure storage of essentials, adjustable waist for a perfect fit, and a stylish, yet practical design. Whether you are hiking through the wilderness or tackling everyday challenges, the Edge Pants keep you equipped and ready for anything. Your gear should work as hard as you do!</p>",
+            "fullRetailSku": "ber-retailSku-2",
+            "sizeChart": "",
+            "actualcolour": "Black",
+            "sizepantswaist": "Waist 32",
+            "sizepantslength": "30",
+            "images": [],
+            "Id": "56299e5a-d22a-45c7-b9bd-b3a9013356ff",
+            "ProductId": "9e5b2673-ef19-4263-8c8a-b3a901300657",
+            "FullSku": "ber-retailSku-2",
+            "Name": "Ber test Rothco Frontline Series Edge Pants-Blk-32-30",
+            "DisplayOrder": 4,
+            "AvailableForPurchase": false,
+            "HideFromListingPage": false,
+            "HideFromPurchaseForms": false,
+            "AvailabilityText": "Out of Stock",
+            "BuyButtonText": "Out of Stock",
+            "Price": 150.00,
+            "PriceAmount": "$150.00",
+            "OriginalPrice": 150.00,
+            "OriginalPriceAmount": "$150.00",
+            "InventoryAvailable": 0
+        }
+    ]
+}
